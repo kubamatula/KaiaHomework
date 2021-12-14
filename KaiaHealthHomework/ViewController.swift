72c3045e06059933d5b93c47df02041be4131700
Jakub Matula <matula@smartpatient.eu>
2021-12-14 07:44:43 +0100

    Initial commit

---
+++ KaiaHealthHomework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KaiaHealthHomework
+//
+//  Created by Jakub Matula on 14/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
